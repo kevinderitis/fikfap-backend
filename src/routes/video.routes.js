8622993f364eb215629858c0c7eb3a886a9407fa
Kevin De Ritis <kevin.deritis@hotmail.com>
2025-11-11 16:20:53 +0700

    Like model import in video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
 import Video from '../models/Video.js';
+import Like from '../models/Like.js';
 import Hashtag from '../models/Hashtag.js';
 import { sanitizeText, extractMentionsAndHashtags } from '../utils/sanitize.js';
 
